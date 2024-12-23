1bfe220928fadc7e82664525df2758c10f1e0a5f
Adam Tsai <adam780930@gmail.com>
2024-12-22 23:21:11 -0800

    updated grid format

--- src/mainpage/Mainpage.js
+++ src/mainpage/Mainpage.js
@@ -12,6 +12,10 @@ function Mainpage() {
     { title: "test title 6", content: "...6" },
   ];
 
+  function getKeyByValue(object, value) {
+    return Object.keys(object).find((key) => object[key] === value);
+  }
+
   useEffect(() => {
     fetch(`https://genshin.jmp.blue/characters/amber`)
       .then((result) => result.json())
@@ -21,30 +25,16 @@ function Mainpage() {
       });
   }, []);
 
-  //   const getInfo = async () => {
-  //     await fetch(`https://genshin.jmp.blue/characters/amber`)
-  //       .then((result) => result.json())
-  //       .then((data) => {
-  //         setRetrievedData(data);
-  //         console.log(data)
-  //       });
-  //   };
-
-  //   getInfo();
-
   return (
-    // <div>
-    //   <div class="text-center border border-primary m-1 p-1 col-2">
-    //     <p class="h4 bg-primary">title</p>
-    //     <p class="h4">...</p>
-    //   </div>
-    //   <div>{testObject[0].title}</div>
-    // </div>
     <div class="container">
       <div class="row">
         {retrievedData.map((el) => (
           <div class="text-center border border-primary m-1 p-1 col-2">
-            <p class="h4 bg-primary">{el.name}</p>
+            <p class="h4 bg-primary">{getKeyByValue(el, el.name)}</p>
+            <p class="h4">{el.name}</p>
+            <p class="h4">{el.vision}</p>
+            <p class="h4">{el.vision}</p>
+            <p class="h4">{el.vision}</p>
             <p class="h4">{el.vision}</p>
           </div>
         ))}
